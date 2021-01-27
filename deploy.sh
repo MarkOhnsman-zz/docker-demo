cff50da71f9eb86e0865a248f9545c18719401ca
Mark R Ohnsman <MarkOhnsman@users.noreply.github.com>
2021-01-27 13:33:43 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 dotnet publish -c Release
-docker build -t TAG_NAME ./bin/Release/netcoreapp5.0/publish
+docker build -t TAG_NAME ./bin/Release/net5.0/publish
 docker tag TAG_NAME registry.heroku.com/HEROKU_APP_NAME/web
 docker push registry.heroku.com/HEROKU_APP_NAME/web
 heroku container:release web -a HEROKU_APP_NAME
